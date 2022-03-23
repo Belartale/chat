80f3c53258b843c2e6441056b87bff10cadb9185
belartale <belartale@gmail.com>
2022-03-23 16:23:37 +0200

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,12 @@ import { UsersModule } from './users/users.module';
 import { MessagesModule } from './messages/messages.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { AppController } from './app.controller';
 
 // 'mongodb://localhost:27017'
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
-      exclude: ['/api*'],
     }),
     UsersModule,
     MessagesModule,
@@ -19,6 +17,5 @@ import { AppController } from './app.controller';
       'mongodb+srv://belartale-chat-admin:belartale-chat-admin@cluster0.pxgec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     ),
   ],
-  controllers: [AppController],
 })
 export class AppModule {}
